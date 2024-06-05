6f7a5f45511bb47d283c5bd63325ac6b0be690d9
Vikas-sm-0017 <vikasvikki4803@gmail.com>
2024-06-05 11:52:14 +0530

    ver 2.0

--- main.c
+++ main.c
@@ -7,6 +7,7 @@ int main()
 {
     int choice;
     char continue_choice;
+    char confirm_choice;
     float price;
 
     do
@@ -15,17 +16,24 @@ int main()
         scanf("%d", &choice);
         processChoice(choice, &price);
 
-        // Display price if valid
+        // Display price and ask for confirmation
         if (price > 0.0)
-{
+        {
             printf("Price: Rs%.2f\n", price);
-}
+            printf("Do you want to confirm this choice? (y/n): ");
+            scanf(" %c", &confirm_choice);
+            if (confirm_choice != 'y' && confirm_choice != 'Y')
+            {
+                printf("You chose to correct your selection.\n");
+                continue;
+            }
+        }
 
         // Ask if user wants to continue
         printf("\nDo you want to continue? (y/n): ");
         scanf(" %c", &continue_choice);
-}
-    while (continue_choice == 'y' || continue_choice == 'Y');
+    }
+    while(continue_choice == 'y' || continue_choice == 'Y');
 
     printf("Exiting the vending machine.\n");
 
