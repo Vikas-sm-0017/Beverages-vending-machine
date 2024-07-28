d68221c4653d37e6d5c76c566656688fe6e5d250
Vikas-sm-0017 <vikasvikki4803@gmail.com>
2024-07-28 16:28:50 +0530

    ver 2.4

--- cofee.h
+++ cofee.h
@@ -1,9 +1,28 @@
+// coffee.h
 
-#ifndef MAIN_H_INCLUDED // Header guard to prevent multiple inclusions
-#define MAIN_H_INCLUDED // Define to mark the start of the header file
-#include<stdio.h>
+#ifndef COFFEE_H
+#define COFFEE_H
 
-void displayMenu();
-void displaySizeOptions(const char *beverage, float smallPrice, float mediumPrice, float largePrice);
-void processChoice(int choice, float *price);
-#endif
+// Define a structure for beverages
+typedef struct {
+    char *name;
+    char *description;
+    float small_price;
+    float medium_price;
+    float large_price;
+    char size_choice;  // Store size choice as char
+    float price;
+    char sugar_choice; // Store sugar choice as char
+} Beverage;
+
+// Function prototypes
+void log_choice_to_file(Beverage beverage, int cups);
+void view_transaction_history();
+int get_valid_integer_input(const char *prompt);
+char get_valid_char_input(const char *prompt, const char *valid_choices);
+float get_valid_float_input(const char *prompt);
+void display_menu();
+void display_size_options(Beverage beverage);
+void process_choice(Beverage *beverages, int choice);
+
+#endif // COFFEE_H
