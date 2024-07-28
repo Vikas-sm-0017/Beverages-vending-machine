d079629e29c82b09180fd2e780c27f58902f7853
Vikas-sm-0017 <vikasvikki4803@gmail.com>
2024-07-28 17:01:52 +0530

    ver 2.4

--- displayMenu.c
+++ displayMenu.c
@@ -1,10 +1,15 @@
-#include "cofee.h"
-void displayMenu()
-{
+// display_menu.c
+
+#include <stdio.h>
+#include "coffee.h"
+
+void display_menu() {
     printf("\nBeverages Vending Machine\n");
     printf("1. Coffee\n");
     printf("2. Tea\n");
     printf("3. Soda\n");
-    printf("4. Exit\n");
-    printf("Enter your choice: ");
+    printf("4. Apple Juice\n");
+    printf("5. Lassi\n");
+    printf("6. View Transaction History\n");
+    printf("7. Exit\n");
 }
